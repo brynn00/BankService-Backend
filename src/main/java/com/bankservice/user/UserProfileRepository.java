2211cb3c479b95572648c2a6fa7ce73389465f3e
sung0368 <by07256@naver.com>
2026-02-01 17:20:25 +0900

    Revert "Merge pull request #1 from brynn00/siwoo"
    This reverts commit 06f0fce07b8849d04429cdb7494b1d8ea24c128f, reversing
    changes made to 2815284d452ba6382e9be158ee1f7bb49cfd3986.

--- src/main/java/com/bankservice/user/UserProfileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bankservice.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserProfileRepository
-        extends JpaRepository<UserProfile, Long> {
-}
-

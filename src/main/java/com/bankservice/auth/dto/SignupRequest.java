8d7adb3d2b2727d4a64a9fa72feaf524ab470669
Yeonsoo Seo <starsoo1213@naver.com>
2026-02-06 18:17:04 +0900

    회원가입 validation 메시지 추가 및 비밀번호 정책 강화

--- src/main/java/com/bankservice/auth/dto/SignupRequest.java
+++ src/main/java/com/bankservice/auth/dto/SignupRequest.java
@@ -1,7 +1,6 @@
 package com.bankservice.auth.dto;
 
 import jakarta.validation.constraints.*;
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,32 +8,52 @@ import lombok.Setter;
 @Setter
 public class SignupRequest {
 
-    @NotBlank
-    @Pattern(regexp = "^[a-zA-Z0-9]{5,20}$")
+    @NotBlank(message = "{userId.notblank}")
+    @Pattern(
+            regexp = "^[a-zA-Z0-9]{5,20}$",
+            message = "{userId.format}"
+    )
     private String userId;
 
 
-    @NotBlank
-    @Email
+    @NotBlank(message = "{email.notblank}")
+    @Email(message = "{email.format}")
     private String email;
 
-    @NotBlank
-    @Size(min = 4, max = 20)
+
+    @NotBlank(message = "{password.notblank}")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$",
+            message = "{password.format}"
+    )
     private String password;
 
-    @NotBlank
-    @Pattern(regexp = "^[가-힣]{2,10}$")
+
+    @NotBlank(message = "{name.notblank}")
+    @Pattern(
+            regexp = "^[가-힣]{2,10}$",
+            message = "{name.format}"
+    )
     private String name;
 
-    @NotBlank
-    @Pattern(regexp = "^\\d{6}-\\d{7}$")
+
+    @NotBlank(message = "{residentNumber.notblank}")
+    @Pattern(
+            regexp = "^\\d{6}-\\d{7}$",
+            message = "{residentNumber.format}"
+    )
     private String residentNumber;
 
-    @NotBlank
-    @Pattern(regexp = "^01[016789]-\\d{3,4}-\\d{4}$")
+
+    @NotBlank(message = "{phone.notblank}")
+    @Pattern(
+            regexp = "^01[016789]-\\d{3,4}-\\d{4}$",
+            message = "{phone.format}"
+    )
     private String phone;
 
-    @NotBlank
-    @Size(max = 255)
+
+    @NotBlank(message = "{address.notblank}")
+    @Size(max = 255, message = "{address.size}")
     private String address;
 }
